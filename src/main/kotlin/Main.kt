0da1496eae1c528d3592b1db928102a6603978ad
Johneryy <johnakintolu4@gmail.com>
2022-11-02 15:15:44 +0100

    Kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,47 @@
+fun main() {
+//    val a = -12
+//    val b = 12
+//    val max = if(a > b) {
+//        println("a is larger than b.")
+//        a
+//    }else{
+//        println("b is larger than a.")
+//        b
+//    }
+//    println("max = $max")
+
+
+
+//    val a = 10
+//    val b = 9
+//    val c = -1
+//    val result : Boolean
+//
+//    result = (a>b) && (a>c)
+//    println(result)
+
+//
+//    val numbers = intArrayOf(1,4,42,-3)
+//    if(4 in numbers) {
+//        println("Numbers array contains 4.")
+//    }
+
+
+//    val name = arrayOf("Ashley","Dorcas","Michel","John","Jummy")
+//    if("Ashley" in name) {
+//        println("It has Ashley")
+//    }else{
+//        println("Can't be found!")
+//    }
+
+
+//
+//    var a = intArrayOf(1,2,3,4,-1)
+//    println(a[1])
+//    a[1] = 12
+//    println(a[1])
+
+
+
+
+}
