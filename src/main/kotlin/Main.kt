e5000fbb43807718e49e99a42838752a4786589f
Johneryy <johnakintolu4@gmail.com>
2022-11-02 16:25:21 +0100

    Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import java.util.Scanner
+
 fun main() {
 //    val a = -12
 //    val b = 12
@@ -43,5 +45,27 @@ fun main() {
 
 
 
+//    val number1: Int = 545344
+//    val number2: Byte = number1.toByte()
+//    println("number 1 =$number1")
+//    println("number 2 =$number2")
 
+
+
+//    print("Enter your name -> ")
+//    val stringInput = readLine()!!
+//    print("Enter your age -> ")
+//    val stringInput1 = readLine()!!
+//    print("Enter your cohort name -> ")
+//    val stringInput2 = readLine()!!
+//    println("My name is  $stringInput")
+//    println("I am $stringInput1 years old")
+//    println("I am in cohort $stringInput2")
+
+
+
+    val reader = Scanner(System.`in`)
+    print("Enter a number -> ")
+    var number:Int= reader.nextInt()
+    println("You entered: $number")
 }
